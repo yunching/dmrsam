3c9c13f66948d3f798ac8398a9a2db7575113db5
Yun Ching Lim <yunching.lim@gmail.com>
2018-11-09 04:53:49 -0500

    Working version

--- R/sat.R
+++ R/sat.R
@@ -2,115 +2,61 @@
 #'
 #' @param mv Vector of market values
 #' @param mv_cap_wts Vector of wts to cap each mv
-#' @param step_amt Dollar amount to distribute at each step
 #' @param debug Toggle if debugging messages should be displayed
 #'
-#' @return
+#' @return A vector of results
 #' @export
 #'
-#' @examples
-market_capping <- function(mv, mv_cap_wts, step_amt=1e6, debug=FALSE){
+#' @examples market_capping(mvs, caps)
+market_capping <- function(mv, capped_wts, debug=FALSE){
   #check mv_cap_wts are valid
-  if (sum(mv_cap_wts < 0 )){
-    stop("There should not be any negative weights in mv_cap_wts.")
+  if (sum(capped_wts < 0 )){
+    stop("There should not be any negative weights in capped_wts")
   }
 
-  if (sum(mv_cap_wts > 100 )){
-    stop("There should not be any weights > 100% in mv_cap_wts.")
+  if (sum(capped_wts > 1 )){
+    stop("There should not be any weights > 100% in capped_wts")
   }
 
   #mv_cap_wts should be same length as mv
-  if (length(mv) != length(mv_cap_wts)){
-    stop("mv and mv_cap_wts should be of same length.")
-  }
-
-  #step_amount cannot be larger than total_mv
-  if (step_amt > sum(mv)){
-    stop("step_amount cannot be larger than sum(mv)")
+  if (length(mv) != length(capped_wts)){
+    stop("mv and capped_mv_wts should be of same length.")
   }
 
   #calculate uncapped weights
   total_mv <- sum(mv)
   uncapped_wts <- mv / total_mv
 
-  #begin capping algorithm
-  #in essence, distribute 1 dollar and check capping
-  #if cap hits, re-distribute to all others
-  #rinse and repeat
-  allocated_mv <- rep(0, length(mv))
-  step_mv <- rep(0, length(mv))
-  step_wts <- uncapped_wts
+  #check if any weight caps are violated
+  mv_caps <- total_mv * capped_wts
+  allocated_mv <- total_mv * uncapped_wts
+  reallocation_wts <- rep(1, length(mv))
 
-  while (sum(allocated_mv) < total_mv) {
-    if (debug==TRUE){
-      message(c("Allocated MV: ", sum(allocated_mv)))
-    }
+  while (sum(allocated_mv > mv_caps) > 0 ){
+    #calculate overallocated mvs
+    overallocated_mv <- allocated_mv - mv_caps
+    overallocated_mv[overallocated_mv <0] <- 0
 
-    #rescale step_wts if they don't add to 1
-    if (sum(step_wts)!=1){
-      step_wts <- step_wts / sum(step_wts)
-    }
+    #calculate wts for re-allocation
+    violated_rows <- which(allocated_mv > mv_caps)
+    unviolated_rows <- which(allocated_mv <= mv_caps)
+    reallocation_wts[violated_rows] <- rep(0, length(violated_rows))
+    reallocation_wts[unviolated_rows] <- uncapped_wts[unviolated_rows] / sum(uncapped_wts[unviolated_rows])
 
-    step_mv = step_amt * step_wts
-    post_step_mv = allocated_mv + step_mv
+    #subtract overallocation
+    allocated_mv <- allocated_mv - overallocated_mv
 
-    #if increment in mv violates capping weights
-    post_step_wts <- post_step_mv / total_mv * 100
-    if (sum(post_step_wts > mv_cap_wts) > 0) {
-      unviolated_rows <- which(post_step_wts <= mv_cap_wts)
-      violated_rows <- which(post_step_wts > mv_cap_wts)
-
-      #figure out what the new step wts should be
-      step_wts[unviolated_rows] <- mv[unviolated_rows] / total_mv
-      step_wts[violated_rows] <- rep(0, length(violated_rows))
-    }
-    else{
-      #if not violated, just increment allocation to each country
-      allocated_mv <- allocated_mv + step_mv
-    }
+    #apply overallocation to remaining countries
+    mv_to_reallocate <- sum(overallocated_mv) * reallocation_wts
+    allocated_mv <- allocated_mv + mv_to_reallocate
   }
 
   capped_mv <- allocated_mv
-  capped_mv_wts <- capped_mv / sum(capped_mv) * 100
-  output <- cbind(mv, mv_cap_wts, uncapped_wts * 100, capped_mv, capped_mv_wts)
-  colnames(output) <- c("mv", "mv_cap_wts", "uncapped_wts", "capped_mv", "capped_mv_wts")
+  capped_mv_wts <- capped_mv / sum(capped_mv)
+  scaling_factors <- capped_mv / mv
+  rescaled_sf <- scaling_factors / max(scaling_factors)
+  output <- cbind(mv, capped_wts, uncapped_wts, capped_mv, capped_mv_wts, scaling_factors, rescaled_sf)
+  colnames(output) <- c("mv", "mv_cap_wts", "uncapped_wts", "capped_mv", "capped_mv_wts", "scaling_factors", "rescaled_sf")
   return(output)
 }
-
-market_capping2 <- function(mv, mv_cap_wts, step_amt=1e6, debug=FALSE){
-  #check mv_cap_wts are valid
-  if (sum(mv_cap_wts < 0 )){
-    stop("There should not be any negative weights in mv_cap_wts.")
-  }
-
-  if (sum(mv_cap_wts > 1 )){
-    stop("There should not be any weights > 100% in mv_cap_wts.")
-  }
-
-  #mv_cap_wts should be same length as mv
-  if (length(mv) != length(mv_cap_wts)){
-    stop("mv and mv_cap_wts should be of same length.")
-  }
-
-  #step_amount cannot be larger than total_mv
-  if (step_amt > sum(mv)){
-    stop("step_amount cannot be larger than sum(mv)")
-  }
-
-  #calculate uncapped weights
-  total_mv <- sum(mv)
-  uncapped_wts <- mv / total_mv
-
-  #if no capping constraints apply at all
-  #then capped constraints = uncapped constraints
-  if (sum(uncapped_wts < mv_cap_wts) == length(mv)){
-    capped_mv_wts <- uncapped_wts
-    capped_mv <- capped_mv_wts * total_mv
-  }
-
-  output <- cbind(mv, mv_cap_wts, uncapped_wts, capped_mv, capped_mv_wts)
-  colnames(output) <- c("mv", "mv_cap_wts", "uncapped_wts", "capped_mv", "capped_mv_wts")
-  return(output)
-}
-
 
--- tests/testthat/test_mv_capping.R
+++ tests/testthat/test_mv_capping.R
@@ -1,48 +1,50 @@
 context("Market value capping")
 
+data(mvs)
+data(caps)
+
 test_that("3 names, no capping",{
-  expect_equal(market_capping(c(1:3), rep(100,3), step=1)[,"capped_mv_wts"], c(1/6, 1/3, 1/2) * 100)
+  expect_equal(market_capping(c(1:3), rep(1,3))[,"capped_mv_wts"], c(1/6, 1/3, 1/2))
 })
 
+test_that("",{
+  expect_equal(market_capping(rep(25, 4), c(0.02, 1, 1, 1))[,"capped_mv_wts"], c(0.02, 0.32666667, 0.32666667, 0.32666667))
+})
+
+test_that("10 names, no capping",{
+  expect_equal(market_capping(1:10, rep(1, 10))[,"capped_mv_wts"], 1:10/55)
+})
 
 test_that("3 names, 1 capped",{
-  expect_equal(market_capping(c(1:3), c(2, 100, 100), step=1)[,"capped_mv_wts"], c(0, 40, 60))
+  expect_equal(market_capping(c(1:3), c(0.02, 1, 1))[,"capped_mv_wts"], c(0.02, 0.392, 0.588))
 })
 
 test_that("3 names, 2 capped",{
-  expect_equal(market_capping(c(1:3), c(2, 2, 100), step=1)[,"capped_mv_wts"], c(0, 0, 10))
+  expect_equal(market_capping(c(1:3), c(0.02, 0.02, 1))[,"capped_mv_wts"], c(0.02, 0.02, 0.96))
 })
 
-#market_capping(1:10, rep(100, 10))
-# market_capping(1:3, c(100, 100, 100), debug=TRUE, step=1)
-# market_capping(rep(25, 4), c(2, 100, 100, 100), debug=TRUE)
+test_that("Sep 2018",{
+  expect_equal(
+    market_capping(mvs, caps)[,"capped_mv_wts"],
+    c(
+      0.01892155,
+      0.02197923,
+      0.01828742,
+      0.09342356,
+      0.06184465,
+      0.05000000,
+      0.15000000,
+      0.01856537,
+      0.02895431,
+      0.05297996,
+      0.08601833,
+      0.39902561
+    )
+  )
+})
 
-mvs<- c(500407906.00,
-        581272702.00,
-        483637513.00,
-        2470721801.00,
-        1635571759.00,
-        2154268200.00,
-        10256050102.00,
-        490988281.00,
-        765738897.00,
-        1401132226.00,
-        2274879731.00,
-        10552812685.00
-)
 
-caps <- c(1,
-          1,
-          1,
-          1,
-          1,
-          0.05,
-          0.15,
-          1,
-          1,
-          0.15,
-          1,
-          1
-)
 
-#market_capping(mvs, caps, debug=TRUE)
+
+
+
